e86b428eae12e8a264803ee2d622c587601e8c75
James <james@jastewart.co.uk>
2020-08-23 18:42:17 +0100

    widing the button

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,7 +11,7 @@
                 <!-- eslint-disable-next-line -->
                 <b>WrestleKind</b> is a UK-based non-profit that provides entertainment for people in need by age, ill-health, disability, or other disadvantage, and the carers who support them. We work with organisations and families to create unforgettable experiences.
             </p>
-            <router-link href="#" to='our-work' class="button big">Our work</router-link>
+            <router-link href="#" to='our-work' class="button big wide">Our work</router-link>
         </div>
         <span class="image object">
             <img src="@/assets/v.jpg" alt />
