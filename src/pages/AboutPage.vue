a279a786c35ca33769746d466ef4d7b028a8d480
James <james@jastewart.co.uk>
2020-11-24 21:37:02 +0000

    text adjustment

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -41,8 +41,7 @@ export default {
           role: 'Founder',
           name: 'Lee Greig',
           img: 'jj.jpg',
-          text:
-                        'Performing as <b>Jack Jester</b>, Lee is a stalwart of the British wrestling scene, with over 16 years of wrestling experience across the UK as a performer, promoter and trainer. As a performer, Lee is best known for his time with Glasgow based ICW, and has been the main promoter of Wrestle Experience Scotland since 2017. Additionally, Jack works with the Asylum wrestling school as a trainer.',
+          text: 'Performing as <b>Jack Jester</b>, Lee is a stalwart of the British wrestling scene, with over 16 years of wrestling experience across the UK as a performer, promoter and trainer. As a performer, Lee is best known for his time with Glasgow based ICW, and has been the main promoter of Wrestle Experience Scotland since 2017. Additionally, Jack works with the Asylum wrestling school as a trainer.',
           socialMedia: {
             twitter: '@GlasgowJester',
             facebook: 'GlasgowJester',
@@ -53,11 +52,11 @@ export default {
           role: 'Founder',
           name: 'Kimberly Benson',
           img: 'v.jpg',
-          text:
-                        'Performing as <b>Viper</b> or <b>Piper Niven</b>, Kim became one of the first Scottish female wrestlers to work for global wrestling giant World Wrestling Entertainment when she joined their NXT UK brand in 2019, having performed in three matches for the company in 2017. A popular international figure, Kim has not only performed the length and breadth of the UK, she has also wrestled in American and Japan. She is currently a full time employee of WWE and also trains wrestlers in Scotland.',
+          text: 'Performing as <b>Viper</b> or <b>Piper Niven</b>, Kim became one of the first Scottish female wrestlers to work for global wrestling giant World Wrestling Entertainment when she joined their NXT UK brand in 2019, having performed in three matches for the company in 2017. A popular international figure, Kim has not only performed the length and breadth of the UK, she has also wrestled in American and Japan. She is currently a full time employee of WWE and also trains wrestlers in Scotland.',
           socialMedia: {
             twitter: '@viperpiperniven',
             facebook: 'ViperWrestling',
+            instagram: 'viperpiperniven',
           },
         },
         {
@@ -65,8 +64,7 @@ export default {
           role: 'Founder',
           name: 'James Stewart',
           img: 'js.png',
-          text:
-                        "James is a software developer and experienced charity professional. James's previous charitable positions include as Chair of the Shetland Skatepark Association (2011-2012) where he led the group to raise £200,000 for a skatepark in the Shetland Islands and as founder and Chair of Get-Well Gamers UK (2014-2019), a charity that delivered video games and consoles to sick kids in hospital.",
+          text: "James is a software developer and experienced charity professional. James's previous charitable positions include as Chair of the Shetland Skatepark Association (2011-2012) where he led the group to raise £200,000 for a skatepark in the Shetland Islands and as founder and Chair of Get-Well Gamers UK (2014-2019), a charity that delivered video games and consoles to sick kids in hospital.",
           socialMedia: {
             twitter: '@JAStewart',
           },
